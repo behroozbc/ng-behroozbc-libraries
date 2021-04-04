84af927bd9e2c38f2e089f7cfda6a40541f0a8ec
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-04-04 19:27:08 +0430

    init validators package

---
+++ projects/validators/src/lib/validators.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ValidatorsComponent } from './validators.component';
+
+
+
+@NgModule({
+  declarations: [
+    ValidatorsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ValidatorsComponent
+  ]
+})
+export class ValidatorsModule { }
